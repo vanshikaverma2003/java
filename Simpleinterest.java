6f7ad11db4bf73fb27b20ec6f90f97112806409c
vanshikaverma2003 <109541924+vanshikaverma2003@users.noreply.github.com>
2022-07-22 00:11:26 +0530

    Add files via upload

---
+++ Simpleinterest.java
@@ -0,0 +1,23 @@
+class Simpleinterest
+{
+public static void main(String args[])
+{
+float principal=200,rate=22,time=4;
+float simple_interest=principal*rate*time/100;
+{
+System.out.println("Enter principal: "+principal);
+}
+{
+System.out.println("Enter rate: "+rate);
+}
+{
+System.out.println("Enter time: "+time);
+}
+{
+System.out.println("Simple_interest: "+simple_interest);
+}
+}
+}
+
+
+

7f757dd860604e405ab8094a2c68c6619bfb90a9
vanshikaverma2003 <109541924+vanshikaverma2003@users.noreply.github.com>
2022-07-21 17:24:12 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,38 @@
+class Datatypes
+{
+public static void main(String args[]){
+int a=10;
+
+System.out.println("value of variable is= " +a+ "size of int" +Integer.BYTES);
+
+char b='v';
+
+System.out.println("value of variable is= " +b+"size of char"+ Character.BYTES);
+
+short c=34;
+
+System.out.println("value of variable is= " +c+ "size of short" +Short.BYTES);
+
+long d=22;
+
+System.out.println("value of variable is= " +d+ "size of long" +Long.BYTES);
+
+double e=131;
+
+System.out.println("value of variable is= " +e+ "size of int" +Double.BYTES);
+
+float i=11.3f;
+
+System.out.println("value of variable is= " +i+ "size of float" +Float.BYTES);
+
+byte g=1;
+
+System.out.println("value of variable is= " +a+ "size of byte" +Byte.BYTES);
+
+boolean h=true;
+
+System.out.println("value of variable is= " +a);
+
+}
+} 
+

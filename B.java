263e634d49127776d062d1cf1012ba54f84f0c2e
vanshika <vanshika1951@gmail.com>
2022-07-21 15:58:05 +0530

    Initial commit

---
+++ B.java
@@ -0,0 +1,17 @@
+class B
+{
+public static void main (String args[])
+{
+int a=100;
+int b=90;
+if(a<b)
+{
+System.out.println("A is less than B");
+}
+else if(a>b)
+{
+System.out.println("A is greater than B");
+}
+}
+}
+
---
+++ Calculator.java
@@ -0,0 +1,12 @@
+class Calculator
+{
+public static void main(String[]args)
+{
+int a=20,b=10;
+System.out.println("Addition: "+(a+b));
+System.out.println("Subtraction: "+(a-b));
+System.out.println("Divison: "+(a/b));
+System.out.println("Multiplication: "+(a*b));
+}
+}
+
